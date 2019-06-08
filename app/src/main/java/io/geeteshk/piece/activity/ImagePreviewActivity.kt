14007e7f337edacfd575b26e9e58ed3a9a84b0f4
Geetesh Kalakoti <kalakotig@gmail.com>
2019-06-07 20:10:00 -0500

    Add empty AugmentImageActivity for viewing pieces

--- app/src/main/java/io/geeteshk/piece/activity/ImagePreviewActivity.kt
+++ app/src/main/java/io/geeteshk/piece/activity/ImagePreviewActivity.kt
@@ -16,8 +16,10 @@
 
 package io.geeteshk.piece.activity
 
+import android.content.Intent
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.transition.Transition
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
@@ -39,7 +41,27 @@ class ImagePreviewActivity : AppCompatActivity() {
     }
 
     private fun setupUi() {
+        augmentImage.shrink(false)
         supportActionBar?.hide()
+        window.sharedElementEnterTransition.addListener(object : Transition.TransitionListener {
+            override fun onTransitionEnd(transition: Transition?) {
+                augmentImage.extend()
+            }
+
+            override fun onTransitionCancel(transition: Transition?) {
+                augmentImage.extend()
+            }
+
+            override fun onTransitionResume(transition: Transition?) {}
+            override fun onTransitionPause(transition: Transition?) {}
+            override fun onTransitionStart(transition: Transition?) {}
+        })
+
+        augmentImage.setOnClickListener {
+            val augmentIntent = Intent(this, AugmentImageActivity::class.java)
+            startActivity(augmentIntent)
+            finish()
+        }
     }
 
     private fun setPreviewImage() {
